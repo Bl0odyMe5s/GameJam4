9f87e0b47a3d8eb48fc0ed99143b8670df335686
Tom <halo3halo@hotmail.com>
2018-11-20 14:36:38 +0100

    Added base networking

---
+++ GameJam4/Assets/Scripts/GameStateManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStateManager : MonoBehaviour {
+    private GameState gameState;
+    public GameState GameState
+    {
+        get
+        {
+            return gameState;
+        }
+        set
+        {
+            gameState = value;
+        }
+    }
+
+    public bool hasAlienSelected;
+}
+
+public enum GameState
+{
+    Menu,
+    Playing
+}
---
+++ GameJam4/Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuController : MonoBehaviour
+{
+    public Button stopConnectingButton;
+
+    public void StartHosting()
+    {
+        CustomNetworkManager.instance.StartHosting();
+    }
+
+    public void StartClient()
+    {
+        CustomNetworkManager.instance.StartClient();
+        stopConnectingButton.gameObject.SetActive(true);
+    }
+
+    public void SetNewIpAdress(string ip)
+    {
+        CustomNetworkManager.instance.networkAddress = ip;
+    }
+
+    public void StopHosting()
+    {
+        CustomNetworkManager.instance.StopHosting();
+        stopConnectingButton.gameObject.SetActive(false);
+    }
+
+    public void SetPlayerName(string name)
+    {
+        CustomNetworkManager.playerName = name;
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
