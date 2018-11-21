d80a8dbbc090361c65225458c933e491466303c9
Tom <halo3halo@hotmail.com>
2018-11-21 15:57:14 +0100

    ja dingen

--- GameJam4/Assets/Alien.cs
+++ GameJam4/Assets/Alien.cs
@@ -4,14 +4,13 @@ using UnityEngine;
 using UnityEngine.Networking;
 
 public class Alien : NetworkBehaviour {
+    public int alienDamage = 40;
 
     private const float MOVE_SPEED = 6;
     private const float ROTATE_SPEED = 3;
     private bool isWalking, isAttacking;
     private Rigidbody rb;
     private Animator animator;
-
-    public Collider hitbox;
 
     // Use this for initialization
     void Start ()
@@ -90,6 +89,23 @@ public class Alien : NetworkBehaviour {
 
     public void DoAttack()
     {
-        
+        if (isLocalPlayer)
+            CmdAttack();
+    }
+
+    [Command]
+    public void CmdAttack()
+    {
+        Collider[] cols = Physics.OverlapBox(transform.position + transform.forward * 0.5f, Vector3.one);
+        for (int i = 0; i < cols.Length; i++)
+        {
+            Transform theRoot = cols[i].transform.root;
+            if (theRoot.CompareTag("Player"))
+            {
+                Debug.Log(cols[i].gameObject.name);
+                PlayerHealth pHealth = theRoot.GetComponent<PlayerHealth>();
+                pHealth.TakeDamage(alienDamage);
+            }
+        }
     }
 }
