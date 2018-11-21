fb9502d1625f960879119ac82fed1be560a23832
Mas Stapel <mas_stapel@hotmail.com>
2018-11-21 10:54:47 +0100

    Made a simple Alien movement script

--- GameJam4/Assets/Alien.cs
+++ GameJam4/Assets/Alien.cs
@@ -4,22 +4,62 @@ using UnityEngine;
 
 public class Alien : MonoBehaviour {
 
-    private const float MOVE_SPEED = 1;
+    private const float MOVE_SPEED = 6;
+    private const float ROTATE_SPEED = 1;
+    private bool isWalking;
+    private Rigidbody rb;
+    private Animator animator;
 
     // Use this for initialization
     void Start ()
     {
-
+        rb = GetComponent<Rigidbody>();
+        animator = GetComponentInChildren<Animator>();
 	}
 	
 	// Update is called once per frame
 	void Update ()
     {
         MoveAlien();
+        AnimateAlien(isWalking);
 	}
 
     private void MoveAlien()
     {
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.A))
+        {
+            isWalking = true;
 
+            if (Input.GetKey(KeyCode.W))
+            {
+                rb.velocity = transform.forward * MOVE_SPEED;
+            }
+
+            else if (Input.GetKey(KeyCode.S))
+            {
+                rb.velocity = -transform.forward * MOVE_SPEED;
+            }
+
+            if (Input.GetKey(KeyCode.D))
+            {
+                transform.Rotate(Vector3.up, ROTATE_SPEED);
+            }
+
+            else if (Input.GetKey(KeyCode.A))
+            {
+                transform.Rotate(Vector3.up, -ROTATE_SPEED);
+            }
+        }
+
+        else
+        {
+            isWalking = false;
+        }
+
+    }
+
+    private void AnimateAlien(bool walkingOrNot)
+    {
+       animator.SetBool("isWalking", walkingOrNot);
     }
 }
